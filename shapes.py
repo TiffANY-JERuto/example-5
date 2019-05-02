48ec958326d0b54aa2eac15ac2064a2b63a747aa
TiffANY-JERuto <47849351+TiffANY-JERuto@users.noreply.github.com>
2019-05-02 19:17:01 +0300

    Python exam practice.
    calculating the area, perimeter, volume and circumference of shapes

---
+++ shapes.py
@@ -0,0 +1,54 @@
+class Circle:
+	def __init__ (self, radius = 7):
+		self.radius = radius
+		self.area = area
+
+
+	def get_area(self):
+		answer = area=3.142*radius^2
+		return answer
+
+	def circumference(self, circumference):
+		answer = 2*3.142*radius
+		return answer
+
+class Square:
+	def __init__ (self, a):
+		self.a = a
+
+	def get_area(self, area):
+		answer = area=a^2
+		return answer
+
+	def perimeter(self, perimeter):
+		answer = perimeter=4*a
+		return answer
+
+class Rectangle:
+	def __init__ (self, w, l):
+		self.w = w
+		self.l = l
+
+	def get_area(self, w, l):
+		answer = area = w*l
+		return answer
+
+	def perimeter (self, w, l):
+		answer = perimeter = 2(w+l)
+		return answer
+
+class Sphere:
+	def __init__ (self, radius):
+		self.radius = radius
+
+	def surface_area(self):
+		answer = area = 4*3.142*radius^2
+		return answer
+
+	def volume(self):
+		answer = 4/3(3.142*radius^3)
+		return answer
+
+
+
+
